67537502a87ec1159c333e3d331f388499c31863
MTahaK <taha.khokhar@mail.utoronto.ca>
2025-06-13 18:18:01 -0400

    Wrote Shader class

--- src/shader.cpp
+++ src/shader.cpp
@@ -128,3 +128,23 @@ void Shader::use() const {
         std::cerr << "[Shader] Cannot use shader, shaderID is 0." << std::endl;
     }
 }
+
+void Shader::setBool(const std::string& name, bool value) const {
+    glUniform1i(glGetUniformLocation(shaderID, name.c_str()), (int)value);
+}
+void Shader::setInt(const std::string& name, int value) const {
+    glUniform1i(glGetUniformLocation(shaderID, name.c_str()), value);
+}
+void Shader::setFloat(const std::string& name, float value) const {
+    glUniform1f(glGetUniformLocation(shaderID, name.c_str()), value);
+}
+void Shader::setVec2(const std::string& name, const glm::vec2& value) const {
+    glUniform2fv(glGetUniformLocation(shaderID, name.c_str()), 1, glm::value_ptr(value));
+}
+void Shader::setVec3(const std::string& name, const glm::vec3& value) const {
+    glUniform3fv(glGetUniformLocation(shaderID, name.c_str()), 1, glm::value_ptr(value));
+}
+void Shader::setMat4(const std::string& name, const glm::mat4& mat) const {
+    glUniformMatrix4fv(glGetUniformLocation(shaderID, name.c_str()), 1, GL_FALSE, glm::value_ptr(mat));
+}
+
