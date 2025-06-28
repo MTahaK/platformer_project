f64496014a5c8c2a8c3e8f19e0c23008337b84b1
MTahaK <taha.khokhar@mail.utoronto.ca>
2025-06-28 18:08:26 -0400

    Wrote TileMap class

---
+++ include/tilemap.hpp
@@ -0,0 +1,48 @@
+#include <glm/glm.hpp>
+#include <vector>
+#include "shader.hpp"
+#include "renderer2d.hpp"
+
+// Define Tile type enum
+enum class TileEnum {
+    EMPTY,
+    SOLID,
+};
+
+struct TileType {
+    TileEnum type; // Type of the tile (e.g., EMPTY, SOLID)
+    glm::vec4 color; // Color of the tile
+};
+
+struct Tile {
+    glm::vec2 position; // Position of the tile in world coordinates
+    TileType tileType; // Type and color of the tile
+};
+
+class Tilemap {
+
+    public:
+        Tilemap(int width, int height, float tilesize);
+
+        Tile& getTile(int x, int y);    // Get tile at position (x, y)
+        bool isSolidTile(int x, int y); // Check if the tile at (x, y) is solid
+
+        void setTile(int x, int y, const TileType& tileType); // Set tile at position (x, y) with a specific type
+
+        void renderTileMap(Shader& shader, Renderer2D& renderer) const; // Render the tilemap using the provided shader and renderer
+
+        // World <-> Grid conversions
+        glm::ivec2 worldToTileIndex(const glm::vec2& pos) const;
+        glm::vec2 tileIndexToWorldPos(int x, int y) const;
+
+         // Getters
+        int getWidth() const { return width_; }
+        int getHeight() const { return height_; }
+        float getTileSize() const { return tilesize_; }
+
+    private:
+        std::vector<std::vector<Tile>> tiles; // 2D vector to hold tiles
+        int width_; // Width of the tilemap in tiles
+        int height_; // Height of the tilemap in tiles
+        float tilesize_; // Size of one tile in world units
+}; 
