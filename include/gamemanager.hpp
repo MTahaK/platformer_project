0f658bbd766b2157b17a4581f44f2f65d2021c89
MTahaK <taha.khokhar@mail.utoronto.ca>
2025-07-16 17:04:04 -0400

    Started work on GameManager class

--- include/gamemanager.hpp
+++ include/gamemanager.hpp
@@ -1,15 +1,50 @@
 #pragma once
 #include <glm/glm.hpp>
+#include "tilemap.hpp"
+#include "physics.hpp"
+#include "renderer2d.hpp"
+#include "shader.hpp"
+#include "window.hpp"
+#include "input.hpp"
+#include "playerobject.hpp"
 
 enum class GameState {
     MENU,
-    LOAD,
+    // LOAD, may not use
     PLAY,
     PAUSE,
     DEAD,
-    WIN
+    WIN,
+    EXIT
 };
 
 class GameManager {
     
+    public:
+        // Default constructor leaves everything uninitialized
+        GameManager() = default;
+
+        // Full initialization constructor
+        GameManager(PlayerObject player, Tilemap tilemap, Physics physics, Renderer2D renderer, Shader shader);
+
+        void runGameLoop();
+        void setState();
+        
+        GameState getState();
+
+        // State-specific routines, includes input handling, physics, rendering for each state
+        void handleMenuState();
+        void handlePlayState();
+        void handlePauseState();
+        void handleDeadState();
+        void handleWinState();
+        void handleExitState();
+
+    private:
+        GameState gameState_ = GameState::MENU;
+        PlayerObject player_;
+        Tilemap tilemap_;
+        Physics physics_;
+        Renderer2D renderer_;
+        Shader shader_;
 };
